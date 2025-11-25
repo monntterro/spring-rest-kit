c2babf2b8fd15e548b13941c7a2f12ae5e4df4bb
monntterro <monntterro@gmail.com>
2025-11-25 18:15:30 +0300

    fix: not found exception

--- src/main/java/space/monntterro/springrestkit/core/exception/NotFoundException.java
+++ src/main/java/space/monntterro/springrestkit/core/exception/NotFoundException.java
@@ -3,7 +3,7 @@ package space.monntterro.springrestkit.core.exception;
 import org.springframework.http.HttpStatus;
 
 public class NotFoundException extends ApiException {
-    public NotFoundException(String reason) {
-        super(HttpStatus.BAD_REQUEST, reason);
+    public NotFoundException(Object id, Class<?> resourceClass) {
+        super(HttpStatus.BAD_REQUEST, "Resource not found with id: " + id.toString() + " and class: " + resourceClass.getSimpleName());
     }
 }
